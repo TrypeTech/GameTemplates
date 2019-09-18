a06dfb8a3961592d7615dac4a26f11ab5b1b0894
stevo <steveharroldart3d@gmail.com>
2019-09-18 11:25:36 -0700

    Unity Project Build

---
+++ GameTemplateProject/GameTemplates/Assets/CameraMotor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour {
+
+    // set position of cam in editor and will be same in code
+
+    private Transform lookAt;
+    private Vector3 startOffset;
+    private Vector3 moveVector;
+	// Use this for initialization
+	void Start () {
+       lookAt = GameObject.FindGameObjectWithTag("Player").transform;
+        startOffset = transform.position - lookAt.position;
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        moveVector = lookAt.position + startOffset;
+
+        // x
+        moveVector.x = 0;
+        // y
+        // limint vertical jump
+        moveVector.y = Mathf.Clamp(moveVector.y, 0, 4);
+        
+        transform.position = moveVector;
+	}
+}
---
+++ GameTemplateProject/GameTemplates/Assets/TempleRun/Scripts/PlayerMotor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour {
+
+    private CharacterController controller;
+    public float speed = 5.0f;
+
+    private Vector3 moveVector;
+    private float verticalVelocity = 0.0f;
+    private float gravity = 12.0f;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveVector = Vector3.zero;
+
+
+        // Gravity
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -0.5f;
+        }
+        else
+        {
+            verticalVelocity -= gravity;
+        }
+
+
+        // X - Left and Right
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+        // Y - Up and Down
+        // moveVector.y = Input.GetAxisRaw("Vertical");
+        moveVector.y = verticalVelocity;
+        // Z - Forward and Backward
+        moveVector.z = speed;
+
+        anim.SetFloat("Forward", speed);
+        controller.Move(moveVector * Time.deltaTime);
+	}
+}
